279d3065b18998f5329369408960f7767f2df26a
Stanislav Verovkin <sverevkin25@gmail.com>
2020-03-22 21:36:28 +0200

    remove keys

--- src/main.js
+++ src/main.js
@@ -9,8 +9,6 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-import * as fb from 'firebase/app';
-
 Vue.config.productionTip = false;
 
 Vue.use( VueResource );
@@ -21,18 +19,6 @@ new Vue(
   {
     router,
     store,
-    render: h => h( App ),
-    created () {
-      fb.initializeApp( {
-          apiKey: "AIzaSyC-XnI83nFFRLFXnG2qx_4v5Y2LfKQ6v2A",
-          authDomain: "corona-app-97beb.firebaseapp.com",
-          databaseURL: "https://corona-app-97beb.firebaseio.com",
-          projectId: "corona-app-97beb",
-          storageBucket: "corona-app-97beb.appspot.com",
-          messagingSenderId: "536445879140",
-          appId: "1:536445879140:web:021147ea5cec2997c57071"
-        }
-      );
-    }
+    render: h => h( App )
   }
 ).$mount( '#app' );
